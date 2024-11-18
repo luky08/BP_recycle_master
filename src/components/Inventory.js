ef0bc99587adad8bfc40300cd9143f8af7a40184
Lukáš Helcl <luky.helcl@gmail.com>
2024-11-18 14:02:42 +0100

    vytvořil jsem komponentu pro inventory

---
+++ src/components/Inventory.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Inventory.css";
+
+const Inventory = () => {
+  return (
+    <div className="right-section">
+      <div className="info-box">
+        <div className="info-row">
+          <div className="picture">
+            <img src="/img/peníze.png" alt="Peníze" />
+          </div>
+          <div className="text">150 kč </div>
+        </div>
+        <div className="info-row">
+          <div className="picture">
+            <img src="/img/plast.png" alt="Plast" />
+          </div>
+          <div className="text">2 kg </div>
+        </div>
+        <div className="info-row">
+          <div className="picture">
+            <img src="/img/papír.png" alt="Plast" />
+          </div>
+          <div className="text">4 kg </div>
+        </div>
+        <div className="info-row">
+          <div className="picture">
+            <img src="/img/sklo.png" alt="Plast" />
+          </div>
+          <div className="text">6 kg </div>
+        </div>
+        <div className="info-row">
+          <div className="picture">
+            <img src="/img/smešný koš.png" alt="Plast" />
+          </div>
+          <div className="text">8 kg </div>
+        </div>
+      </div>
+      <button className="purchase-button">Koupit odpad - 10 kg / 15 kč</button>
+    </div>
+  );
+};
+
+export default Inventory;
