add4dee95d6fc46b9c0d419d1d06370453aa8afb
Lukáš Helcl <luky.helcl@gmail.com>
2024-11-18 16:52:05 +0100

    inventory list je hotov a vypisuje se na každé stránce

--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -1,43 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InventoryList.css";
 
 const Inventory = () => {
+  //Proměné
+  const [trash, setTrash] = useState(0);
+  const [money, setMoney] = useState(50);
+  const plastic_waste = useState(50);
+  const glass_waste = useState(50);
+  const paper_waste = useState(50);
+
+  const addTrash = () => {
+    if (money < 15) {
+      alert("Nemáš dostatek peněz!");
+      return; // Zastaví provádění funkce
+    }
+    setTrash(trash + 10); // Zvýší množství odpadu
+    setMoney(money - 15); // Sníží množství peněz
+  };
+
   return (
     <div className="right-section">
-      hej funguju
       <div className="info-box">
         <div className="info-row">
           <div className="picture">
             <img src="/img/peníze.png" alt="Peníze" />
           </div>
-          <div className="text">150 kč </div>
+          <div className="text">{money} kč </div>
         </div>
         <div className="info-row">
           <div className="picture">
             <img src="/img/plast.png" alt="Plast" />
           </div>
-          <div className="text">2 kg </div>
+          <div className="text">{plastic_waste} kg </div>
         </div>
         <div className="info-row">
           <div className="picture">
             <img src="/img/papír.png" alt="Plast" />
           </div>
-          <div className="text">4 kg </div>
+          <div className="text">{paper_waste} kg </div>
         </div>
         <div className="info-row">
           <div className="picture">
             <img src="/img/sklo.png" alt="Plast" />
           </div>
-          <div className="text">6 kg </div>
+          <div className="text">{glass_waste} kg </div>
         </div>
         <div className="info-row">
           <div className="picture">
             <img src="/img/smešný koš.png" alt="Plast" />
           </div>
-          <div className="text">8 kg </div>
+          <div className="text">{trash} kg </div>
         </div>
       </div>
-      <button className="purchase-button">Koupit odpad - 10 kg / 15 kč</button>
+      <button className="purchase-button" onClick={addTrash}>
+        Koupit odpad - 10 kg / 15 kč
+      </button>
     </div>
   );
 };
