1e9639e193b6431f6c5a6c76ed10bb0de09ff4d6
Lukáš Helcl <luky.helcl@gmail.com>
2024-11-18 16:55:16 +0100

    poslední commit v této větvi,upravoval jsem sorting a první funkce

--- src/pages/SortingStation.js
+++ src/pages/SortingStation.js
@@ -3,17 +3,38 @@ import "./SortingStation.css";
 import InventoryList from "../components/InventoryList.js";
 
 const SortingStation = () => {
+  const sorting_items = [
+    {
+      id: 1, // Jedinečné ID předmětu
+      image: "/img/popelar-celej.png",
+      level: 1, // Hodnota úrovně
+      trashAmount: 100, // Množství odpadu
+      time: "00:05:00", // Čas ve formátu hh:mm:ss
+      price: 50, // Cena předmětu
+    },
+    {
+      id: 1, // Jedinečné ID předmětu
+      image: "/img/popelar-celej.png",
+      level: 1, // Hodnota úrovně
+      trashAmount: 100, // Množství odpadu
+      time: "00:05:00", // Čas ve formátu hh:mm:ss
+      price: 50, //
+    },
+  ];
+
   return (
     <div className="sorting-station content">
       <div className="left-section">
         <div className="main-row">
           <div className="left">
-            <img src="/img/popelar-celej.png" alt="Převoz odpadu" />
+            <img src={sorting_items.image} alt="Převoz odpadu" />
           </div>
           <div className="middle">
             <div className="text">
-              <div>lvl1 - Převoz odpadu do třídírny</div>
-              <div>5 kg - 3 min</div>
+              <div>{sorting_items.level} - Převoz odpadu do třídírny</div>
+              <div>
+                {sorting_items.trashAmount} kg - {sorting_items.time} min
+              </div>
             </div>
             <div className="progress-bar">
               <div className="progress"></div>
@@ -21,10 +42,10 @@ const SortingStation = () => {
           </div>
           <div className="right">
             <img src="/img/upgrade-text.png" alt="Upgrade" />
-            <div className="price"> 25 kč</div>
+            <div className="price"> {sorting_items.price} kč</div>
           </div>
         </div>
-        <div className="main-row text"> Připraveno 32 kg</div>
+        {/* <div className="main-row text"> Připraveno 32 kg</div>
         <div className="main-row">
           <div className="left">
             <img src="/img/tridicka.png" alt="Třídění odpadu" />
@@ -42,7 +63,7 @@ const SortingStation = () => {
             <img src="/img/upgrade-text.png" alt="Upgrade" />
             <div className="price"> 50 kč</div>
           </div>
-        </div>
+        </div> */}
       </div>
       <InventoryList />
     </div>
